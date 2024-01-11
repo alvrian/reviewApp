c12bb032bc5a193fbc58321c6adee884c05bb36c
alvrian <alvrian.timotius@gmail.com>
2024-01-11 12:58:27 +0700

    update

--- screens/about.js
+++ screens/about.js
@@ -1,20 +1,13 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
+import { globalStyles } from "../styles/global";
 
 export default function Home() {
     return(
-        <View style = {styles.container}>
+        <View style = {globalStyles.container}>
             <Text>about</Text>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        padding: 24,
-
-    },
-
-})
 
 
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,20 +1,14 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
+import { globalStyles } from "../styles/global";
 
 export default function Home() {
     return(
-        <View style = {styles.container}>
+        <View style = {globalStyles.container}>
             <Text>ReviewDetails Screen</Text>
         </View>
     )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        padding: 24,
-        
-    },
-
-})
 
 
